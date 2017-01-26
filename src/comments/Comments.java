daecbc396c2c652c75ab646624f9940bffa59053
Frick112 <sf137412@students.davenport.k12.ia.us>
2017-01-26 16:59:55 -0600

    asdf

---
+++ src/comments/Comments.java
@@ -0,0 +1,10 @@
+package comments;
+import javax.swing.JOptionPane;
+public class Comments 
+{
+	public static void main(String[] args)
+	{
+		JOptionPane.showMessageDialog(null, "Program comments are nonexicuting statements that you "
+				+ "\nadd to a file for the purpose of documentation");
+	}
+}
